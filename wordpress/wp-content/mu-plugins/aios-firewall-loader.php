b2e134ec534d1dd60419ed22c09cc83b0936264a
orfeaspg <orfpou527@gmail.com>
2025-10-26 20:19:34 +0200

    commit

---
+++ wordpress/wp-content/mu-plugins/aios-firewall-loader.php
@@ -0,0 +1,6 @@
+<?php
+// Begin AIOWPSEC Firewall
+if (file_exists('C:/xampp/htdocs/wordpress/aios-bootstrap.php')) {
+	include_once('C:/xampp/htdocs/wordpress/aios-bootstrap.php');
+}
+// End AIOWPSEC Firewall
